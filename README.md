159db7b0fcd85889f6859d7468c5f3fb2033ed40
Daebis <dae.anthony@gmail.com>
2020-11-01 13:15:25 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,147 @@
+# UnionRolistes.fr
+Fonctionnalité du Bot Discord De UnionRolistes.fr, opensources
+
+# Installation
+1. You need node installed on your system, detailled instructions for different operating systems can be found [here](https://nodejs.org/en/download/).
+
+2. Check your node version with ```node -v``` . Node.js 12.0.0 or newer is required.
+
+3. Cloning the project via git. OneLine Update
+```cd /usr/local/src/```
+```sudo git clone https://github.com/UnionRolistes/Bot-Roles BotRolesList && cd ./BotRolesList/ && sudo npm install && sudo npm run install```
+```pm2 status && pm2 delete 0 && pm2 start index.js```
+
+-> Default Directory
+-> Create a new folder
+-> Clone the project in the folder you just created.
+-> Go into the folder where you cloned the project. 
+
+-
+
+Before setting up the "real" bot you must check some things:
+ - You need a so called token for your bot. You need it later.
+
+ -> How to obtain the token? Create an [application](https://discord.com/developers/applications) and follow the steps below:
+
+Now navigate to "Bot"
+
+![Bot](https://raw.githubusercontent.com/UnionRolistes/Bot-Roles/main/2020-10-17-165755_1920x1080_scrot.png)
+
+![Obtain the token](https://raw.githubusercontent.com/UnionRolistes/Bot-Roles/main/2020-10-17-170011_1920x1080_scrot.png)
+- Since Discord restricted the access to specific user data, you need to enable the so called Privileged Gateway Intents. To so, scroll down and enable both of them And click "Copy"
+
+- A suitable prefix. There is no sense in choosing one which another bot already utilises. So choose a unique one.
+
+4. Install the packages with ```sudo npm install```
+
+5. Execute ```sudo npm run install``` to create a configuration file **before** the first startup of the bot.
+- prefix : This will be the prefix for which the bot will listen(I suggest $).
+- token : Token which you obtained earlier.
+- intents : **Don't touch the next options!** If you're interested why check out this [discord post](https://blog.discord.com/the-future-of-bots-on-discord-4e6e050ab52e).
+ 
+6. Run the bot ```node index.js```.
+
+
+-----------------------------------------
+## Keeping it online with PM2
+-----------------------------------------
+PM2 is an advanced, production process manager for Node.JS.
+
+[The Documentation can be found here.](https://pm2.keymetrics.io/)
+
+Install it globally: 
+```js
+sudo npm install pm2 -g
+```
+Now navigate to the folder with the bot. ( cd /.....)
+```js
+ pm2 start index.js
+```
+You can also specify more options:
+
+```js
+# Specify an app name
+--name <app_name>
+
+# Watch and Restart app when files change
+--watch
+
+# Set memory threshold for app reload
+--max-memory-restart <200MB>
+
+# Specify log file
+--log <log_path>
+
+# Pass extra arguments to the script
+-- arg1 arg2 arg3
+
+# Delay between automatic restarts
+--restart-delay <delay in ms>
+
+# Prefix logs with time
+--time
+
+# Do not auto restart app
+--no-autorestart
+
+# Specify cron for forced restart
+--cron <cron_pattern>
+
+# Attach to application log
+--no-daemon
+
+```
+Now everything is done! Check the status of pm2 with
+```js
+$ pm2 status
+```
+-----------------------------------------
+Managing processes
+
+Managing application state is simple here are the commands:
+```js
+pm2 restart app_name
+pm2 reload app_name
+pm2 stop app_name
+pm2 delete app_name
+
+```
+
+Instead of app_name you can pass:
+
+- all to act on all processes
+- id to act on a specific process id
+-----------------------------------------
+
+
+For more informations check the documentation -> https://pm2.keymetrics.io/ and the quickstart guide -> https://pm2.keymetrics.io/docs/usage/quick-start/
+
+-----------------------------------------
+# Updating the code
+-----------------------------------------
+First of all we have to stop the process manager pm2
+Execute: ```pm2 status``` to get the id or the name of your app
+(Since we only have one here, the id should 0 and the name should be index)
+
+Now stop the process with ```pm2 stop (name/id)```
+in this case -> ```pm2 stop index```
+
+Now update the code in the respective folder via Git.
+go into the folder: cd....
+```git clone https://github.com/Myst82015/L-Union-des-R-listes.git```
+
+Then ```npm run install``` (just in case I added another npm module)
+
+-----------------------------------------
+Update on 16.10.2020: IMPORTANT: To fix the problems with the incorrect membercounts go to setup guide above and check the intents (below point 4). Then regenerate the config ```npm run install```( in the folder where the code is in), don't change anything at the preselected intents!
+-----------------------------------------
+
+You're nearly done!
+
+Navigate in the folder with the bot (cd ./....)
+and daemonize it with pm2
+```pm2 start index.js```
+
+Finally check the status of the process with ```pm2 status```.
+
+
---
+++ index.js
@@ -0,0 +1,153 @@
+process.on('unhandledRejection', err => {
+  if (err.code === 50006) return; // DiscordAPIError: Cannot send an empty message
+  console.log(err.code)
+    console.error('ERROR', `Uncaught Promise Error: \n${err.stack}`);
+});
+
+const Discord = require('discord.js');
+const wait = require('util').promisify(setTimeout);
+const { prefix, token , intents} = require('./config.json');
+const { version } = require('./package.json')
+const fs = require("fs");
+const fetch = require('node-fetch');
+
+const client = new Discord.Client({
+  ws: {
+    intents: intents
+  },
+  disableMentions: 'everyone'
+});
+
+client.once('ready', () => {
+  console.log(`Ready - Logged in as ${client.user.tag}`);
+  // Set the client user's activity
+client.user.setActivity('$help', { type: 'WATCHING' }).then(presence => console.log(`Activity set to ${presence.activities[0].name}`)).catch(console.error);
+});
+
+client.on ('message', async message => {
+	if (message.content === `${prefix}ping`) {
+
+		message.channel.send('Ping?').then(m => m.edit(`Pong!\n:white_small_square: Latency is \`${m.createdTimestamp - message.createdTimestamp}\`ms. \n:white_small_square:API Latency is \`${Math.round(client.ws.ping)}\`ms`));
+	} else if (message.content === `${prefix}credit`) {
+
+    message.channel.send(`${client.user.tag} - Version ${version}\n==============================\n[Owner]\n Dae#5125 (Discord ID: 209065408857243648) \n==============================\n[Developer]\n ◢◤Myst◢◤#4217 (Discord ID: 263022860551847936) \n==============================\n[Other contributors]\n * dryas#5722 \n * Tonitch#2192\n -> Thanks for helping with the formulaire-jdr. `, { code: "asciidoc" });
+    
+    // message.channel.send(`${client.user.tag} - Version \`${version}\`\nThis bot was created by \`◢◤Myst◢◤#4217\` (Discord ID: 263022860551847936) for \`Dae#5125\` (Discord ID: 209065408857243648)\nThanks to \`dryas#5722\` and \`Tonitch#2192\` for helping with the formulaire-jdr.`);
+  } else if (message.content === `${prefix}help`) {
+
+		message.channel.send(`Commands:\n:white_small_square: \`${prefix}roles (channel)\` - Displays all roles of the server. If a channel is given the bot returns a list of roles with access to the channel.\n:white_small_square: \`${prefix}credit\` - Informations about the bot developer(s).\n:white_small_square: \`${prefix}ping\` - Pong!`);
+	} else if (message.content.startsWith(prefix + 'roles')) {
+    
+     if(!message.guild) return; // return it in case it comes from a direct message
+     // We need the arguments for the sub command
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    // Search the channel in the guild, accepts channel mention, ID or the name of the channel
+    const channel = message.mentions.channels.first() || message.guild.channels.cache.find(ch => ch.name === args[1]) || message.guild.channels.cache.find(ch => ch.id === args[1]);
+    if(channel){
+      let rolesmsg = [];
+      let index = 0;
+      rolesmsg[index] = "";
+
+      message.guild.roles.cache.sort(
+        (h,l) => l.position - h.position
+        ).forEach(role => {
+          if(!channel.permissionsFor(role).toArray().includes('VIEW_CHANNEL')) return;
+          //rolesmsg[index] += `${role.name} (${role.id})`;
+          rolesmsg[index] += `${role.name}`;
+          for (let i = role.name.length; i < 25; i++) {
+          rolesmsg[index] += " ";
+          }
+          rolesmsg[index] += "::  " + role.members.size + "\n";
+
+          if (rolesmsg[index].length > 1800) {
+              index++;
+              rolesmsg[index] = "";
+          }
+      });
+      message.channel.send(`▫️ Roles which have access to ${channel}.\n\`Name :: Members with role\``)
+      message.channel.send()
+      for (let i = 0; i < rolesmsg.length; i++) {
+        await wait(3000); // Wait 3 seonds between each message due to specific ratelimits on the server
+          message.channel.send(rolesmsg[i], { code: "asciidoc" });    
+      }
+      return;
+    }
+
+
+     /**
+     * Posts input to hastebin
+     * @param {(Object|string)} input   Input as object or string
+     * @param {string} extension        File type e.g. js, txt
+     * @returns Hastebin Url
+     */
+    function hastebin(input, extension) {
+      return new Promise(function(res, rej) {
+        if (!input) rej('[Error] Missing Input');
+        fetch('https://hasteb.in/documents', { method: 'POST', body: input })
+          .then(res => res.json())
+          .then(body => {
+            res('https://hasteb.in/' + body.key + ((extension) ? '.' + extension : ''));
+          }).catch(e => rej(e));
+      });
+    }
+    
+    let rolelist = await message.guild.roles.cache.sort((h,l) => h.position - l.position).map(role => `▫️ ${role.name} - ${role.members.size} Member(s)`).reverse().join("\n")
+    const url = await hastebin(`Role list for ${message.guild.name}\n\n${message.guild.roles.cache.size} roles in total with ${message.guild.memberCount} members in total.\n\nFormat: Role name(Type: String) - Members with role(Type: Number)\n\n${rolelist}`, 'txt').catch(err => console.log(err.stack));
+    
+    let embed = new Discord.MessageEmbed()
+    .setDescription(`▫✅Uploaded the list to hasteb.in\n[Click here](${url})`)
+    message.channel.send(embed)
+    
+    // Format new Date() to something readable
+    var today = new Date();
+    var dd = today.getDate();
+    
+    var mm = today.getMonth()+1; 
+    var yyyy = today.getFullYear();
+    if(dd<10) 
+    {
+        dd='0'+dd;
+    } 
+    
+    if(mm<10) 
+    {
+        mm='0'+mm;
+    } 
+    today = mm+'-'+dd+'-'+yyyy;
+    // console.log(today);
+    const roles = message.guild.roles.cache.sort((h,l) => h.position - l.position).map(role => role = {name: role.name, id: role.id, membercount: role.members.size}).reverse()
+
+    fs.writeFileSync(`./logs/rolelist_${today}.txt`, JSON.stringify(roles, null, '\t')); // Indented with tab);
+    // Attach it to the message
+    const attachment = new Discord.MessageAttachment(`./logs/rolelist_${today}.txt`);
+    // Send the attachment in the message channel with a content
+     message.channel.send(`${message.author} Here is the raw list.`, attachment);
+    
+     let rolesmsg = [];
+        let index = 0;
+        rolesmsg[index] = "";
+
+        message.guild.roles.cache.sort(
+          (h,l) => l.position - h.position
+          ).forEach(role => {
+            //rolesmsg[index] += `${role.name} (${role.id})`;
+            rolesmsg[index] += `${role.name}`;
+            for (let i = role.name.length; i < 25; i++) {
+            rolesmsg[index] += " ";
+            }
+            rolesmsg[index] += "::  " + role.members.size + "\n";
+
+            if (rolesmsg[index].length > 1800) {
+                index++;
+                rolesmsg[index] = "";
+            }
+        });
+
+        for (let i = 0; i < rolesmsg.length; i++) {
+          await wait(3000); // Wait 3 seonds between each message due to specific ratelimits on the server
+            message.channel.send(rolesmsg[i], { code: "asciidoc" });    
+        }
+	}
+});
+
+client.login(token);
