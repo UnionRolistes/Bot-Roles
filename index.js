88397a00c53042f17c4486f22cd0d60b2a846cac
Myst <34489470+Myst82015@users.noreply.github.com>
2020-11-04 00:03:16 +0100

    Update index.js
    - Added hasteb.in url when a channel is specified
    - Changed the filter of the roles
    -> now the role needs the permission to send messages in the specified channel instead of simply view it
    - COde structure slightly changed no big deal (moved hastebin function to the top & embed contructur also moved upwards)

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 process.on('unhandledRejection', err => {
-  if (err.code === 50006) return; // DiscordAPIError: Cannot send an empty message
+  // if (err.code === 50006) return; // DiscordAPIError: Cannot send an empty message
   console.log(err.code)
-    console.error('ERROR', `Uncaught Promise Error: \n${err.stack}`);
+  console.error('ERROR', `Uncaught Promise Error: \n${err.stack}`);
 });
 
 const Discord = require('discord.js');
@@ -37,43 +37,8 @@ client.on ('message', async message => {
 
 		message.channel.send(`Commands:\n:white_small_square: \`${prefix}roles (channel)\` - Displays all roles of the server. If a channel is given the bot returns a list of roles with access to the channel.\n:white_small_square: \`${prefix}credit\` - Informations about the bot developer(s).\n:white_small_square: \`${prefix}ping\` - Pong!`);
 	} else if (message.content.startsWith(prefix + 'roles')) {
-    
-     if(!message.guild) return; // return it in case it comes from a direct message
-     // We need the arguments for the sub command
-    const args = message.content.slice(prefix.length).trim().split(/ +/g);
-    // Search the channel in the guild, accepts channel mention, ID or the name of the channel
-    const channel = message.mentions.channels.first() || message.guild.channels.cache.find(ch => ch.name === args[1]) || message.guild.channels.cache.find(ch => ch.id === args[1]);
-    if(channel){
-      let rolesmsg = [];
-      let index = 0;
-      rolesmsg[index] = "";
 
-      message.guild.roles.cache.sort(
-        (h,l) => l.position - h.position
-        ).forEach(role => {
-          if(!channel.permissionsFor(role).toArray().includes('VIEW_CHANNEL')) return;
-          //rolesmsg[index] += `${role.name} (${role.id})`;
-          rolesmsg[index] += `${role.name}`;
-          for (let i = role.name.length; i < 25; i++) {
-          rolesmsg[index] += " ";
-          }
-          rolesmsg[index] += "::  " + role.members.size + "\n";
-
-          if (rolesmsg[index].length > 1800) {
-              index++;
-              rolesmsg[index] = "";
-          }
-      });
-      message.channel.send(`▫️ Roles which have access to ${channel}.\n\`Name :: Members with role\``)
-      message.channel.send()
-      for (let i = 0; i < rolesmsg.length; i++) {
-        await wait(3000); // Wait 3 seonds between each message due to specific ratelimits on the server
-          message.channel.send(rolesmsg[i], { code: "asciidoc" });    
-      }
-      return;
-    }
-
-     /**
+    /**
      * Posts input to hastebin
      * @param {(Object|string)} input   Input as object or string
      * @param {string} extension        File type e.g. js, txt
@@ -91,11 +56,55 @@ client.on ('message', async message => {
       });
     }
     
-    let rolelist = await message.guild.roles.cache.sort((h,l) => h.position - l.position).map(role => `▫️ ${role.name} - ${role.members.size} Member(s)`).reverse().join("\n")
-    const url = await hastebin(`Role list for ${message.guild.name}\n\n${message.guild.roles.cache.size} roles in total with ${message.guild.memberCount} members in total.\n\nFormat: Role name(Type: String) - Members with role(Type: Number)\n\n${rolelist}`, 'txt').catch(err => console.log(err.stack));
+     if(!message.guild) return; // return it in case it comes from a direct message
+
+     let embed = new Discord.MessageEmbed()
     
-    let embed = new Discord.MessageEmbed()
-    .setDescription(`▫✅Uploaded the list to hasteb.in\n[Click here](${url})`)
+     // We need the arguments for the sub command
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    // Search the channel in the guild, accepts channel mention, ID or the name of the channel
+    const channel = message.mentions.channels.first() || message.guild.channels.cache.find(ch => ch.name === args[1]) || message.guild.channels.cache.find(ch => ch.id === args[1]);
+    if(channel){
+      let rolesmsg = [];
+      let index = 0;
+      rolesmsg[index] = "";
+
+      message.guild.roles.cache.sort(
+        (h,l) => l.position - h.position
+        ).forEach(role => {
+          if(!channel.permissionsFor(role).toArray().includes('SEND_MESSAGES')) return;
+          //rolesmsg[index] += `${role.name} (${role.id})`;
+          rolesmsg[index] += `${role.name}`;
+          for (let i = role.name.length; i < 25; i++) {
+          rolesmsg[index] += " ";
+          }
+          rolesmsg[index] += "::  " + role.members.size + "\n";
+
+          if (rolesmsg[index].length > 1800) {
+              index++;
+              rolesmsg[index] = "";
+          }
+      });
+      let rolelist = await message.guild.roles.cache.filter(role => channel.permissionsFor(role).toArray().includes('VIEW_CHANNEL')).sort((h,l) => h.position - l.position).map(role => `▫️ ${role.name} - ${role.members.size} Member(s)`).reverse().join("\n")
+      let url = await hastebin(`Role list for ${message.guild.name}\n\n${message.guild.roles.cache.size} roles in total with ${message.guild.memberCount} members in total.\n\nFormat: Role name(Type: String) - Members with role(Type: Number)\n\n${rolelist}`, 'txt').catch(err => console.log(err.stack));
+      embed.setDescription(`✅Uploaded the list to hasteb.in\n[Click here](${url})`)
+      message.channel.send(embed)
+
+      message.channel.send(`▫️ Roles which have access to ${channel}.\n\`Name :: Members with role\``)
+
+      for (let i = 0; i < rolesmsg.length; i++) {
+        await wait(3000); // Wait 3 seonds between each message due to specific ratelimits on the server
+          message.channel.send(rolesmsg[i], { code: "asciidoc" });    
+      }
+      return;
+    }
+
+     
+    
+    let rolelist = await message.guild.roles.cache.sort((h,l) => h.position - l.position).map(role => `▫️ ${role.name} - ${role.members.size} Member(s)`).reverse().join("\n")
+    let url = await hastebin(`Role list for ${message.guild.name}\n\n${message.guild.roles.cache.size} roles in total with ${message.guild.memberCount} members in total.\n\nFormat: Role name(Type: String) - Members with role(Type: Number)\n\n${rolelist}`, 'txt').catch(err => console.log(err.stack));
+    
+    embed.setDescription(`✅Uploaded the list to hasteb.in\n[Click here](${url})`)
     message.channel.send(embed)
     
     // Format new Date() to something readable
