61e9909d62a335a153c89468693bd4994ea932e6
Myst <34489470+Myst82015@users.noreply.github.com>
2021-01-14 14:12:40 +0100

    Clean up
    Cleaned up the code a bit
    Pushed the version to 0.0.7
    Updated discord.js dependency  to 12.5.1

--- index.js
+++ index.js
@@ -26,17 +26,16 @@ client.user.setActivity('$help', { type: 'WATCHING' }).then(presence => console.
 
 client.on ('message', async message => {
 	if (message.content === `${prefix}ping`) {
-
 		message.channel.send('Ping?').then(m => m.edit(`Pong!\n:white_small_square: Latency is \`${m.createdTimestamp - message.createdTimestamp}\`ms. \n:white_small_square:API Latency is \`${Math.round(client.ws.ping)}\`ms`));
-	} else if (message.content === `${prefix}credit`) {
-
+  
+  } else if (message.content === `${prefix}credit`) {
     message.channel.send(`${client.user.tag} - Version ${version}\n==============================\n[Owner]\n Dae#5125 (Discord ID: 209065408857243648) \n==============================\n[Developer]\n ◢◤Myst◢◤#4217 (Discord ID: 263022860551847936) \n==============================\n[Other contributors]\n * dryas#5722 \n * Tonitch#2192\n -> Thanks for helping with the formulaire-jdr. `, { code: "asciidoc" });
-    
     // message.channel.send(`${client.user.tag} - Version \`${version}\`\nThis bot was created by \`◢◤Myst◢◤#4217\` (Discord ID: 263022860551847936) for \`Dae#5125\` (Discord ID: 209065408857243648)\nThanks to \`dryas#5722\` and \`Tonitch#2192\` for helping with the formulaire-jdr.`);
+  
   } else if (message.content === `${prefix}help`) {
-
-		message.channel.send(`Commands:\n:white_small_square: \`${prefix}roles (channel)\` - Displays all roles of the server. If a channel is given the bot returns a list of roles with access to the channel.\n:white_small_square: \`${prefix}credit\` - Informations about the bot developer(s).\n:white_small_square: \`${prefix}ping\` - Pong!`);
-	} else if (message.content.startsWith(prefix + 'roles')) {
+    message.channel.send(`Commands:\n:white_small_square: \`${prefix}roles (channel)\` - Displays all roles of the server. If a channel is given the bot returns a list of roles with access to the channel.\n:white_small_square: \`${prefix}credit\` - Informations about the bot developer(s).\n:white_small_square: \`${prefix}ping\` - Pong!`);
+  
+  } else if (message.content.startsWith(prefix + 'roles')) {
 
     /**
      * Posts input to hastebin
@@ -92,17 +91,15 @@ client.on ('message', async message => {
       embed.setDescription(`✅Uploaded the list to hastebin.com\n[Click here](${url})`)
       message.channel.send(embed)
 
-      message.channel.send(`▫️ Roles which have access to ${channel}.\n\`Name :: Members with role\``)
-
-      for (let i = 0; i < rolesmsg.length; i++) {
+      // message.channel.send(`▫️ Roles which have access to ${channel}.\n\`Name :: Members with role\``)
+      /*for (let i = 0; i < rolesmsg.length; i++) {
         await wait(3000); // Wait 3 seonds between each message due to specific ratelimits on the server
           message.channel.send(rolesmsg[i], { code: "asciidoc" });    
       }
+      */
       return;
     }
-
-     
-    
+   
     let rolelist = await message.guild.roles.cache.sort((h,l) => h.position - l.position).map(role => `▫️ ${role.name} - ${role.members.size} Member(s)`).reverse().join("\n")
     let url = await hastebin(`Role list for ${message.guild.name}\n\n${message.guild.roles.cache.size} roles in total with ${message.guild.memberCount} members in total.\n\nFormat: Role name(Type: String) - Members with role(Type: Number)\n\n${rolelist}`, 'txt').catch(err => console.log(err.stack));
     
@@ -132,7 +129,7 @@ client.on ('message', async message => {
     const attachment = new Discord.MessageAttachment(`./logs/rolelist_${today}.txt`);
     // Send the attachment in the message channel with a content
      message.channel.send(`${message.author} Here is the raw list.`, attachment);
-    
+    /*
      let rolesmsg = [];
         let index = 0;
         rolesmsg[index] = "";
@@ -151,14 +148,15 @@ client.on ('message', async message => {
               
                 index++;
                 rolesmsg[index] = "";
-            } */
+            } 
         });
 
         for (let i = 0; i < rolesmsg.length; i++) {
           await wait(3000); // Wait 3 seonds between each message due to specific ratelimits on the server
             message.channel.send(rolesmsg[i], { code: "asciidoc" });    
-        }
-	}
+        } */
+  }
+  
 });
 
 client.login(token);
