b6584318aeb645e13fba519dbe6acd0a035bd84c
Myst <34489470+Myst82015@users.noreply.github.com>
2022-11-26 17:52:12 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,37 @@
+String.prototype.toProperCase = function() {
+	return this.toLowerCase().replace(/(^|[\s.])[^\s.]/gm, (s) => s.toUpperCase());
+};
+
+const Client = require('./src/structures/Client');
+const { Intents } = require('discord.js');
+require('dotenv').config();
+
+const client = module.exports = new Client({ 
+	devs: ['263022860551847936'],
+	config: './config',
+	clientOptions: { 
+			intents: [
+				"Guilds",
+				"GuildMessages",
+				"GuildPresences",
+				"GuildMessageReactions",
+				"DirectMessages",
+				"MessageContent",
+				"GuildVoiceStates"
+			]}
+});
+client.run();
+
+process.on('exit', (code) => {
+	client.logger.error(__filename, `The process is exiting with code ${code}. Terminating....`);
+});
+const RoleUpdater = require('./src/modules/RoleUpdater');
+RoleUpdater(client);
+
+const { Events } = require('./src/structures/EventHandler');
+const directory = `${__dirname}/src/events/`;
+new Events(client, directory);
+
+// const gRPCClient = require('./src/microservices/passwordTest/client')
+
+client.login(process.env.discord_bot_token);
