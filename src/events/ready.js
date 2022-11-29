ae48708ade2a8cafb7abed8f9dd3a672fecd0d89
Myst <34489470+Myst82015@users.noreply.github.com>
2022-11-29 21:58:57 +0100

    Update ready.js
    Rnmove relics from a long gone time

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,7 +1,5 @@
 const DB = require('../modules/DBManager');
 const { ActivityType } = require('discord.js');
-// const WhiteList = require('../assets/whitelist.json');
-
 
 class Event {
 	constructor() {
@@ -16,20 +14,16 @@ class Event {
 		// Setting the status
 		this.bot.user.setActivity('Coding', { type: ActivityType.Competing });
 
-
 		// Check for uncached users and guilds without a document in the database
 		setInterval(() => {
 			this.bot.guilds.cache.forEach(async guild => {
-				// await guild.members.fetch().catch(console.error);
 				await DB.fetchGuild(guild.id);
 			});
 		}, 300000);
 
 		setInterval(() => {
 			this.bot.user.setActivity('Coding', { type: ActivityType.Competing });
-			// .catch(console.error);
 		}, 300000);
-
 
 	}
 }
