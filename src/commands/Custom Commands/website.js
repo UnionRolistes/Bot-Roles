df16b6341b374a964a302f61cfe08760645771b7
Myst <34489470+Myst82015@users.noreply.github.com>
2022-12-18 15:17:48 +0100

    Create website.js

---
+++ src/commands/Custom Commands/website.js
@@ -0,0 +1,34 @@
+const Command = require('../../structures/Command');
+const { ApplicationCommandType } = require('discord.js');
+
+
+class Website extends Command {
+	constructor(client) {
+		super({
+			name: 'website',
+			description: 'Envoie un lien pour accéder le site web.',
+			usage: '',
+			category: 'Custom Commands',
+			aliases: [],
+			cooldown: 3,
+			requiredPerms: 'READ_MESSAGES',
+			enabled: true,
+			ownerOnly: false,
+			guildOnly: true,
+			args: false,
+			type: ApplicationCommandType.ChatInput,
+			defaultMemberPermissions: 'SendMessages',
+			slashOptions: [],
+
+		});
+
+		this.client = client;
+	}
+
+	async execute(client, interaction, language) {
+		await interaction.reply({ content: '\`The essence of a role playing game is that it is a group, cooperative experience. (Gary Gygax)\`\n[Take a look at our website.](http://site.unionrolistes.fr/)', ephemeral: true });
+	}
+
+}
+
+module.exports = Website;
