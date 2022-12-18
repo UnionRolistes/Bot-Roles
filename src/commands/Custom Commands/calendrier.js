5dbd60bf38ff32d318247ab8a712268f4a697b81
Myst <34489470+Myst82015@users.noreply.github.com>
2022-12-18 15:17:38 +0100

    Create calendrier.js

---
+++ src/commands/Custom Commands/calendrier.js
@@ -0,0 +1,34 @@
+const Command = require('../../structures/Command');
+const { ApplicationCommandType } = require('discord.js');
+
+
+class Calendrier extends Command {
+	constructor(client) {
+		super({
+			name: 'calendrier',
+			description: 'Envoie un lien pour accéder au calendrier.',
+			usage: '',
+			category: 'Conception',
+			aliases: [],
+			cooldown: 3,
+			requiredPerms: 'READ_MESSAGES',
+			enabled: true,
+			ownerOnly: false,
+			guildOnly: true,
+			args: false,
+			type: ApplicationCommandType.ChatInput,
+			defaultMemberPermissions: 'SendMessages',
+			slashOptions: [],
+
+		});
+
+		this.client = client;
+	}
+
+	async execute(client, interaction, language) {
+		await interaction.reply({ content: 'The calender can be found [here](http://planning.unionrolistes.fr/Calendar).', ephemeral: true });
+	}
+
+}
+
+module.exports = Calendrier;
