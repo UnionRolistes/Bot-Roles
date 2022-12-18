9823525b6a493c21be4fa45f05fb4bac263a65bd
Myst <34489470+Myst82015@users.noreply.github.com>
2022-12-18 15:17:41 +0100

    Create jdr.js

---
+++ src/commands/Custom Commands/jdr.js
@@ -0,0 +1,34 @@
+const Command = require('../../structures/Command');
+const { ApplicationCommandType } = require('discord.js');
+
+
+class JDR extends Command {
+	constructor(client) {
+		super({
+			name: 'jdr',
+			description: 'Envoie un lien pour créer une partie.',
+			usage: '',
+			category: 'Conception',
+			aliases: [],
+			cooldown: 3,
+			requiredPerms: 'READ_MESSAGES',
+			enabled: true,
+			ownerOnly: false,
+			guildOnly: true,
+			args: false,
+			type: ApplicationCommandType.ChatInput,
+			defaultMemberPermissions: 'SendMessages',
+			slashOptions: [],
+
+		});
+
+		this.client = client;
+	}
+
+	async execute(client, interaction, language) {
+		await interaction.reply({ content: 'Crée une partie [here]( http://planning.unionrolistes.fr/?webhook=https://discord.com/api/webhooks/873043760307642408/IM1VX44JlYfElbBOlVrYhUKM47uzW7Bp5jm7tp3xVxKYnE3wuzbbMZ8nGfhTT_uLEfLw).', ephemeral: true });
+	}
+
+}
+
+module.exports = JDR;
