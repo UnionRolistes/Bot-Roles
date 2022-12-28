e61059b4017946cc28c045fac6da51d27d45965c
Myst <34489470+Myst82015@users.noreply.github.com>
2022-12-29 00:17:15 +0100

    Update botinfo.js

--- src/commands/Utility/botinfo.js
+++ src/commands/Utility/botinfo.js
@@ -1,7 +1,9 @@
 /* eslint-disable no-useless-escape */
 const Command = require('../../structures/Command');
-const { EmbedBuilder, ApplicationCommandType } = require('discord.js');
+const {ApplicationCommandType } = require('discord.js');
 const { version } = require('../../../package.json');
+
+
 
 class Botinfo extends Command {
 	constructor(client) {
@@ -16,68 +18,94 @@ class Botinfo extends Command {
 
 			type: ApplicationCommandType.ChatInput,
 			defaultMemberPermissions: 'ManageRoles',
-			slashOptions: [
-				{
-					name: 'add',
-					description: 'Add role to a user.',
-					type: 1,
-					options: [
-						{
-							name: 'role',
-							description: 'The role you want to add to the user.',
-							type: 8,
-							required: true,
-						},
-						{
-							name: 'user',
-							description: 'The user you want to add role to.',
-							type: 6,
-							required: true,
-						},
-					],
-				},
-			],
+			slashOptions: [],
 
 		});
 
 		this.client = client;
 	}
 
-	async execute(client, interaction, language) {
-		const date = new Date(client.uptime);
-		let strDate = '';
-		strDate += date.getUTCDate() - 1 + ` ${language.getKey('word_time_day_plural')}, `;
-		strDate += date.getUTCHours() + ` ${language.getKey('word_time_hour_plural')}, `;
-		strDate += date.getUTCMinutes() + ` ${language.getKey('word_time_minute_plural')}, `;
-		strDate += date.getUTCSeconds() + ` ${language.getKey('word_time_second_plural')}`;
-		const devUsernames = [];
-		await client.devs.forEach(async id => {
-			const user = await client.users.fetch(id);
-			devUsernames.push(`${user.username}#${user.discriminator}`);
-		});
+	async execute(client, interaction) {
 
-		const embed = new EmbedBuilder()
-			// .setTitle(`${language.getKey('words_offical_project_title')}`)
-			.setTitle('O_o')
-			.addFields([
-				{ name: language.getKey('words_general_informations'), value: `
-				${language.getKey('word_developer')}: \`${devUsernames.join('\`, \`')}\`
-				${language.getKey('word_version')}: \`${version}\`
-				${language.getKey('word_uptime')}: \`${strDate}\`
+		interaction.reply({ content:
+		`\`\`\`asciidoc
+Hello, World!
+==============================
+Projet:: Bot_Roles
+Version:: ${version}
+Developer:: Myst#4217
+Contributors:: 
+* Dae#5125
+* dryas#5722 
+* Tonitch#2192\`\`\`` });
+		/*
+Hello, World!
+============
+Author Name, <author@domain.foo>
 
-				` },
-				{ name: language.getKey('word_links') + ':', value: '[Invite Bot](https://discordapp.com/oauth2/authorize?client_id=' + client.user.id + `&scope=bot&permissions=537250816) - [${language.getKey('words_support_server')}](https://discord.gg/Sn92FSj) ` },
-			])
+you can write text http://example.com[with links], optionally
+using an explicit link:http://example.com[link prefix].
 
-			.setImage('https://forum.star-conflict.com/uploads/monthly_2021_01/500-250-logo.png.f24a71acb2fc8e2b85a18c485a1af565.png')
-			.setColor('#36393F')
-			.setThumbnail(client.user.displayAvatarURL({ dynamic: true, size: 2048 }))
-			.setFooter({ text: `${client.user.tag}`, iconURL: client.user.displayAvatarURL() })
-			.setTimestamp();
+* single quotes around a phrase place 'emphasis'
+** alternatively, you can put underlines around a phrase to add _emphasis_
+* astericks around a phrase make the text *bold*
+* pluses around a phrase make it +monospaced+
+* `smart' quotes using a leading backtick and trailing single quote
+** use two of each for double ``smart'' quotes
 
-		interaction.reply({ embeds: [embed] });
+- escape characters are supported
+- you can escape a quote inside emphasized text like 'here\'s johnny!'
+
+term:: definition
+ another term:: another definition
+
+// this is just a comment
+
+Let's make a break.
+
+'''
+
+////
+we'll be right with you
+
+after this brief interruption.
+////
+
+== We're back!
+
+Want to see a image::images/tiger.png[Tiger]?
+
+.Nested highlighting
+++++
+<this_is inline="xml"></this_is>
+++++
+
+____
+asciidoc is so powerful.
+____
+
+another quote:
+
+[quote, Sir Arthur Conan Doyle, The Adventures of Sherlock Holmes]
+____
+When you have eliminated all which is impossible, then whatever remains, however improbable, must be the truth.
+____
+
+Getting Literal
+---------------
+
+ want to get literal? prefix a line with a space.
+
+....
+I'll join that party, too.
+....
+
+. one thing (yeah!)
+. two thing `i can write code`, and `more` wipee!
+
+NOTE: AsciiDoc is quite cool, you should try it.
+		*/
 	}
-
 }
 
 module.exports = Botinfo;
