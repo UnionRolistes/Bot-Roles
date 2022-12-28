9d77979de3a8778e59a45d0fb2ac739eb6ef8ba5
Myst <34489470+Myst82015@users.noreply.github.com>
2022-12-29 00:56:02 +0100

    Update User.js

--- src/Schema/User.js
+++ src/Schema/User.js
@@ -2,7 +2,6 @@ const { Schema, model } = require('mongoose');
 
 const User = new Schema({
 	id: String,
-	language: { type: String, default: 'en-US' },
 	guest: { type: Boolean, default: false },
 });
 
