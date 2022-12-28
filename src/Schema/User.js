7924ef069a22c79fb156b310f2e2e62e7d056ad1
Myst <34489470+Myst82015@users.noreply.github.com>
2022-12-29 00:18:00 +0100

    Update User.js

--- src/Schema/User.js
+++ src/Schema/User.js
@@ -2,10 +2,8 @@ const { Schema, model } = require('mongoose');
 
 const User = new Schema({
 	id: String,
-	messageCount: { type: Number, default: 0 },
 	language: { type: String, default: 'en-US' },
 	guest: { type: Boolean, default: false },
-
 });
 
 module.exports = model('User', User);
