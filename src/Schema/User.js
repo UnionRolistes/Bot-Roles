a8fdc6c4b5608b71cf6302dc67f1a40d9f72638e
Myst <34489470+Myst82015@users.noreply.github.com>
2022-12-29 13:11:08 +0100

    Update User.js

--- src/Schema/User.js
+++ src/Schema/User.js
@@ -2,7 +2,7 @@ const { Schema, model } = require('mongoose');
 
 const User = new Schema({
 	id: String,
-	guest: { type: Boolean, default: false },
+	tosAccepted: { type: Boolean, default: false },
 });
 
 module.exports = model('User', User);
