31341e5c0987c7076a7977955c9a511418a2d5bf
Myst <34489470+Myst82015@users.noreply.github.com>
2023-01-05 13:23:37 +0100

    Update ready.js

--- src/events/ready.js
+++ src/events/ready.js
@@ -14,6 +14,13 @@ class Event {
 		// Setting the status
 		this.bot.user.setActivity('Coding', { type: ActivityType.Competing });
 
+		// Check for uncached users and guilds without a document in the database
+		setInterval(() => {
+			this.bot.guilds.cache.forEach(async guild => {
+				// await DB.fetchGuild(guild.id);
+			});
+		}, 300000);
+
 		setInterval(() => {
 			this.bot.user.setActivity('Coding', { type: ActivityType.Competing });
 		}, 300000);
