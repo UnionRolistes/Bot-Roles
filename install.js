11bb5cf6eca971f96f54c321b23cd99647395ba0
Myst <34489470+Myst82015@users.noreply.github.com>
2026-07-12 11:53:03 +0200

    Fix install.js
    Fiexd install.js for the new version of inquirer/prompts
    Keeping OLDinstall.js for reference

--- install.js
+++ install.js
@@ -1,60 +1,46 @@
-const inquirer = require('inquirer');
+const { input, password } = require("@inquirer/prompts");
+
 const fs = require('fs');
 
-const prompts = [
-	{
-		name: 'token',
-		type: 'password',
-		message: 'Enter your auth-token from discord.com/developer/applications 🔑:',
-		validate: function(value) {
-			if (value.length) {
-				return true;
-			}
-			else {
-				return 'Please enter your valid token!';
-			}
-		},
-	},
-	{
-		name: 'intents',
-		type: 'checkbox',
+async function main() {
+    console.log('-----------------------------------------');
+    const token = await input({
+        message: "Enter your auth-token from discord.com/developer/applications 🔑:",
+        mask: false, 
+        validate(value) {
+            return value.length > 0 || "Please enter your valid token!";
+        }
+    });
+        const mongoUrl = await input({
+        message: "Enter the mongo DB url:"
+    });
+        const dbName = await input({
+        message: "Enter the database name:"
+    });
+    const clientId = await input({
+        message: "Enter the Client ID of the bot application:"
+    });
+    const guildId = await input({
+        message: "Enter a  Guild ID for quick deployment of new slash commands:"
+    });
 
-		message: 'Which intents would you like? \n',
+    const envContent = `
+# Discord Parameters
+    discord_bot_token=${token}
+    
+# Database
+    mongo_url=${mongoUrl}
+    dbName=${dbName}
 
-		choices: [
-			{ 'name': 'Guilds', 'value': 'GUILDS', 'checked': true },
-			{ 'name': 'Guild Messages', 'value': 'GUILD_MESSAGES', 'checked': true },
-			{ 'name': 'Direct Messages', 'value': 'DIRECT_MESSAGES' },
-			{ 'name': 'Guild Members (privileged)', 'value': 'GUILD_MEMBERS', 'checked': true },
-			{ 'name': 'Guild Bans', 'value': 'GUILD_BANS' },
-			{ 'name': 'Guild Emojis', 'value': 'GUILD_EMOJIS' },
-			{ 'name': 'Guild Integrations', 'value': 'GUILD_INTEGRATIONS' },
-			{ 'name': 'Guild Webhooks', 'value': 'GUILD_WEBHOOKS' },
-			{ 'name': 'Guild Invites', 'value': 'GUILD_INVITES' },
-			{ 'name': 'Guild Voice States', 'value': 'GUILD_VOICE_STATES' },
-			{ 'name': 'Guild Message Reactions', 'value': 'GUILD_MESSAGE_REACTIONS' },
-			{ 'name': 'Guild Message Typing', 'value': 'GUILD_MESSAGE_TYPING' },
-			{ 'name': 'Direct Message Reactions', 'value': 'DIRECT_MESSAGE_REACTIONS' },
-			{ 'name': 'Direct Message Typing', 'value': 'DIRECT_MESSAGE_TYPING' },
-			{ 'name': 'Guild Presences (privileged)', 'value': 'GUILD_PRESENCES', 'checked': true },
-		],
+# Development Parameters
+    clientId=${clientId}
+    guildId=${guildId}
+    `;
 
-	},
+    fs.writeFileSync(".env", envContent, "utf8");
+    console.log('-----------------------------------------');
+    console.log('Configuration has been written.');
+}
 
-];
-
-(async function() {
-	console.log('-----------------------------------------');
-
-	const answers = await inquirer.prompt(prompts);
-
-	let baseConfig = '{{token}}';
-	baseConfig = baseConfig
-		.replace('{{token}}', `"${answers.token}"`)
-		.replace('{{intents}}', JSON.stringify(answers.intents));
-
-	fs.writeFileSync('./.envtest', baseConfig);
-
-	console.log('-----------------------------------------');
-	console.log('Configuration has been written.');
-}());
+    
+main();
