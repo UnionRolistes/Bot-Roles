c4581fbe5e28eab9739f5e7a3a25505b80b982c9
Myst <34489470+Myst82015@users.noreply.github.com>
2026-07-17 17:41:21 +0200

    Update DBManager.js
    Remove unused code and removed unnecessary calls for the UserSchema

--- src/modules/DBManager.js
+++ src/modules/DBManager.js
@@ -1,90 +1,5 @@
 const mongoose = require('mongoose');
 
-// ALL methods related to Guilds will be removed in a future update since they are not required anymore.
-/*
-/**
- * Creates a new guild document in the database from the guilds collection
- * @param guildID A valid discord guild id
- * @returns {Object} Guild Return the newly created Guild object
- */
-/*
-async function createGuild(guildID) {
-	if(!guildID) return 'no guild id provided';
-	if(typeof guildID !== 'string') return 'provided guild id isnt a string';
-	const existingGuild = await mongoose.models.Guild.findOne({ id: guildID });
-	if(existingGuild) return 'The guild already exists.';
-	const Guild = new mongoose.models.Guild({
-		id: guildID,
-		configuration: {
-			prefix: '*',
-		},
-	}).save();
-	return Guild;
-}
-
-/**
- * Deletes a guild document in the database from the guilds collection
- * @param guildID A valid discord guild id
- * @returns Guild Return the deleted Guild object
- */
-/*
-async function deleteGuild(guildID) {
-	if(!guildID) return 'no guild id provided';
-	if(typeof guildID !== 'string') return 'provided guild id isnt a string';
-	const guildToDelete = await mongoose.models.Guild.findOne({ id: guildID });
-	if(!guildToDelete) return 'The guild doesn\'t exists.';
-	if(guildToDelete) await mongoose.models.Guild.findOneAndDelete({ id: guildID });
-
-	return guildToDelete;
-}
-
-/**
- * Fetches a guild document in the database from the guilds collection
- * @param guildID A valid discord guild id
- * @returns Guild Return the fetched Guild object
- */
-/*
-async function fetchGuild(guildID) {
-	if(!guildID) return 'no guild id provided';
-	if(typeof guildID !== 'string') return 'provided guild id isnt a string';
-	const guildToFetch = await mongoose.models.Guild.findOne({ id: guildID });
-	if(!guildToFetch) return await createGuild(guildID);
-
-	return guildToFetch;
-}
-
-/**
- *
- * @param {String} guildID User to update
- * @param {String} query Query to update
- * @param {String/Boolean} change The actual change to the database
- * @param {Boolean} isEvents If the events are updated
- * @returns {Object} User
- */
-/*
-async function updateGuild(guildID, query, change, isEvents = false) {
-	if(!guildID) return 'no guild id provided';
-	if(typeof guildID !== 'string') return 'provided user id isnt a string';
-
-	if(!query) return 'no query provided';
-	if(!change) return 'no change provided';
-	if(change == '--reset') {
-		const Guild = await mongoose.models.Guild.findOneAndUpdate({ id: guildID }, { $set: { [query]: null } });
-
-		return Guild;
-	}
-	if(isEvents) {
-		const eventQuery = `events.${query}`;
-		return await mongoose.models.Guild.findOneAndUpdate({ id: guildID }, { $set: { [eventQuery]: change } });
-	}
-
-	const Guild = await mongoose.models.Guild.findOneAndUpdate({ id: guildID }, { $set: { [query]: change } });
-
-	return Guild;
-}
-*/
-// USER
-
 /**
  * Creates a new user document in the database, users collection
  * @param userID A valid discord user id
@@ -117,19 +32,19 @@ async function deleteUser(userID) {
 }
 
 /**
- * Retrieves a user doument frpm the database or creates a new one if it doesn't exist.
+ * Retrieves a user doument from the database or creates a new one if it doesn't exist.
  * @param {String} userID discord id from the user to fetch
  * @returns {Object} userToFetch User document
  */
 
-async function fetchUser(userID, apcreateIfNotPresent = false) {
+async function fetchUser(userID, createIfNotPresent = false) {
 	if(!userID) return 'no user id provided';
 	if(typeof userID !== 'string') return 'provided user id isnt a string';
-	const UserSchema = require('../Schema/User');
-	const userToFetch = await UserSchema.findOne({ id: userID });
-	// const userToFetch = await mongoose.models.User.findOne({ id: userID });
-	if(!userToFetch && apcreateIfNotPresent) return await createUser(userID);
-	if(!userToFetch && !apcreateIfNotPresent) return false;
+	// const UserSchema = require('../Schema/User');
+	// const userToFetch = await UserSchema.findOne({ id: userID });
+	const userToFetch = await mongoose.models.User.findOne({ id: userID });
+	if(!userToFetch && createIfNotPresent) return await createUser(userID);
+	if(!userToFetch && !createIfNotPresent) return false;
 
 	return userToFetch;
 }
