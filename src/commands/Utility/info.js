9f63c753e55f905078f9c7c3f25d0d402ed43de2
Myst <34489470+Myst82015@users.noreply.github.com>
2026-08-16 12:48:34 +0200

    Update info.js
    New ANSI design

--- src/commands/Utility/info.js
+++ src/commands/Utility/info.js
@@ -1,4 +1,4 @@
-/* eslint-disable no-useless-escape */
+/* eslint-disable no-inline-comments */
 const Command = require('../../structures/Command');
 const { ActionRowBuilder, ButtonBuilder, ButtonStyle, EmbedBuilder, ApplicationCommandType, MessageFlags } = require('discord.js');
 const { version } = require('../../../package.json');
@@ -35,12 +35,12 @@ class Botinfo extends Command {
 
 	async execute(client, interaction, language) {
 
-// ANSI Color Code
-const orange = '\u001b[33m'; // orange
-const white  = '\u001b[37m'; // white
-const reset  = '\u001b[0m'; //escape
+		// ANSI Color Code
+		const orange = '\u001b[33m'; // orange
+		const white = '\u001b[37m'; // white
+		const reset = '\u001b[0m'; // escape
 
-const description = `\`\`\`ansi
+		const description = `\`\`\`ansi
 ${orange}UR-Role-Stat:${reset}
 ${language.botinfo.projectDescription}
 -------
@@ -52,27 +52,27 @@ ${orange}${language.botinfo.developer}${reset}: ${white}@terimor${reset}
 ${orange}${language.botinfo.contributors}${reset}: ${white}@fr_dae, dryas#5722, Tonitch#2192${reset}
 \`\`\``;
 
-const embed = new EmbedBuilder()
-	.setThumbnail(this.client.user.displayAvatarURL())
-	.setDescription(description)
-	.setColor('#36393F');
+		const embed = new EmbedBuilder()
+			.setThumbnail(this.client.user.displayAvatarURL())
+			.setDescription(description)
+			.setColor('#36393F');
 
-const row = new ActionRowBuilder().addComponents(
-  new ButtonBuilder()
-    .setLabel('Discord')
-    .setStyle(ButtonStyle.Link)
-    .setURL('https://discord.gg/rxqqBS8'),
-  new ButtonBuilder()
-    .setLabel(language.botinfo.website)
-    .setStyle(ButtonStyle.Link)
-    .setURL('http://unionrolistes.fr/'),
-  new ButtonBuilder()
-    .setLabel('Github')
-    .setStyle(ButtonStyle.Link)
-    .setURL('https://github.com/UnionRolistes'),
-);
+		const row = new ActionRowBuilder().addComponents(
+			new ButtonBuilder()
+				.setLabel('Discord')
+				.setStyle(ButtonStyle.Link)
+				.setURL('https://discord.gg/rxqqBS8'),
+			new ButtonBuilder()
+				.setLabel(language.botinfo.website)
+				.setStyle(ButtonStyle.Link)
+				.setURL('http://unionrolistes.fr/'),
+			new ButtonBuilder()
+				.setLabel('Github')
+				.setStyle(ButtonStyle.Link)
+				.setURL('https://github.com/UnionRolistes'),
+		);
 
-await interaction.reply({ embeds: [embed], components: [row] });
+		await interaction.reply({ embeds: [embed], components: [row] });
 
 	}
 }
